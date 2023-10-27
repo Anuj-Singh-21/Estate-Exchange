adf5a49cb7d2ff0916c4eb75d4b2b94ec36ad02c
Anuj Singh <anujsingh21092000@gmail.com>
2023-10-27 15:02:41 +0530

    Made all required Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Signin from "./Pages/Signin";
+import Profile from "./Pages/Profile";
+import SignUp from "./Pages/SignUp";
 
 export default function App() {
-  return <div className="text-red-500">Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
